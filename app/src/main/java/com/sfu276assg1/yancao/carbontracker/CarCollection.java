8d9c8f9f22a1ce09e7e56ac3eabfba2a55633710
yancao <caoyanc@sfu.ca>
2017-02-27 10:55:41 -0800

    Initial Commit -- several emptyActivity and JavaClass added

---
+++ app/src/main/java/com/sfu276assg1/yancao/carbontracker/CarCollection.java
@@ -0,0 +1,8 @@
+package com.sfu276assg1.yancao.carbontracker;
+
+/**
+ * Created by yancao on 2017-02-27.
+ */
+
+public class CarCollection {
+}
---
+++ app/src/main/java/com/sfu276assg1/yancao/carbontracker/DisplayCarbonFootprintActivity.java
@@ -0,0 +1,13 @@
+package com.sfu276assg1.yancao.carbontracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayCarbonFootprintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_carbon_footprint);
+    }
+}
