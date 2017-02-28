7854af87d9367ec736f01fc5be96552ff70cecec
Vu Nguyen <vun@sfu.ca>
2017-02-28 13:59:37 -0800

    Create Pie Chart
    Implemented a small part of Journey class in order to test the Pie Chart. The Pie Chart is working now, but still need some more work.

--- app/src/main/java/com/sfu276assg1/yancao/carbontracker/DisplayCarbonFootprintActivity.java
+++ app/src/main/java/com/sfu276assg1/yancao/carbontracker/DisplayCarbonFootprintActivity.java
@@ -1,13 +1,63 @@
 package com.sfu276assg1.yancao.carbontracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DisplayCarbonFootprintActivity extends AppCompatActivity {
 
+    private ArrayList<Journey> list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_carbon_footprint);
+
+        generateDataForChart();
+        generatePieChart();
+    }
+
+    private void generatePieChart() {
+        List<PieEntry> yEntries = new ArrayList<>();
+        List<String> xEntries = new ArrayList<>();
+        for(int i = 0; i < list.size(); i++) {
+            yEntries.add(new PieEntry(list.get(i).getNumCarbon(), list.get(i).getCarName()));
+        }
+
+        for(int i = 0; i < list.size(); i++) {
+            xEntries.add(list.get(i).getCarName());
+        }
+
+
+        PieDataSet dataSet = new PieDataSet(yEntries, "Car Type");
+        dataSet.setSliceSpace(2);
+        dataSet.setValueTextSize(12);
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData data = new PieData(dataSet);
+
+        PieChart chart = (PieChart)findViewById(R.id.Chart);
+        Description description = new Description();
+        description.setText("Amount of Carbon per Car");
+        chart.setDescription(description);
+        chart.setRotationEnabled(true);
+        chart.setHoleRadius(25f);
+        chart.setTransparentCircleAlpha(0);
+        chart.setData(data);
+        chart.animateY(2000);
+        chart.invalidate();
+    }
+
+    private void generateDataForChart() {
+        Intent intent = getIntent();
+        list = (ArrayList<Journey>)intent.getSerializableExtra("Array List");
     }
 }
--- app/src/main/java/com/sfu276assg1/yancao/carbontracker/Journey.java
+++ app/src/main/java/com/sfu276assg1/yancao/carbontracker/Journey.java
@@ -1,8 +1,37 @@
 package com.sfu276assg1.yancao.carbontracker;
 
+import java.io.Serializable;
+
 /**
  * Created by yancao on 2017-02-27.
  */
 
-public class Journey {
+public class Journey implements Serializable {
+    public Journey(float numCarbon, String carName, String routeName) {
+        this.numCarbon = numCarbon;
+        this.carName = carName;
+        this.routeName = routeName;
+    }
+
+    public float getNumCarbon() {
+        return numCarbon;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public String getRouteName() {
+        return routeName;
+    }
+
+    @Override
+    public String toString() {
+        return carName + " " + routeName + " " + numCarbon;
+    }
+
+    private float numCarbon;
+    private String carName;
+    private String routeName;
+
 }
