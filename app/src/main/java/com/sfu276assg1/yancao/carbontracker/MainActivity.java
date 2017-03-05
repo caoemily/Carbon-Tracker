174d121a5689455021147d1f5c677652bcd43c59
Steven Ho <xuanhung1017@gmail.com>
2017-03-04 16:14:51 -0800

    add addJourney() into AddRouteActivity

--- app/src/main/java/com/sfu276assg1/yancao/carbontracker/MainActivity.java
+++ app/src/main/java/com/sfu276assg1/yancao/carbontracker/MainActivity.java
@@ -1,13 +1,10 @@
 package com.sfu276assg1.yancao.carbontracker;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
         setUpAddJourneytButton();
         setUpViewDataButton();
+
     }
 
     private void setUpAddJourneytButton() {
