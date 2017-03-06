a52b9f316ce4fe79981fae7eeaf385e9d6574495
yancao <caoyanc@sfu.ca>
2017-03-05 21:38:34 -0800

    Car Activities edit upon Jane's code.
    Able to show saved car and let users to choose make/model/year
    Need to fix: 1. hard coding on Strings; 2. indication for class at the beginning of the classes; 3. csv file read; 4. emission file for car from csv file.
    Note: For populating the dropdown menus, hard coding is used. And fake make,model,year data are shown.

--- app/src/main/java/com/sfu276assg1/yancao/carbontracker/AddCarActivity.java
+++ app/src/main/java/com/sfu276assg1/yancao/carbontracker/AddCarActivity.java
@@ -1,13 +1,136 @@
 package com.sfu276assg1.yancao.carbontracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AddCarActivity extends AppCompatActivity {
+
+    String[] carMake ={"a","b","c"};
+    String[] carModel ={"d","e","f"};
+    String[] carYear ={"1","2","3"};
+    Car currentCar = new Car();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_car);
+
+        setupAcceptButton();
+        setupSinpper();
     }
+
+    private void setupSinpper() {
+
+        Spinner makeSpin = (Spinner) findViewById(R.id.car_make);
+        Spinner modelSpin = (Spinner) findViewById(R.id.car_model);
+        Spinner yearSpin = (Spinner) findViewById(R.id.car_year);
+
+        ArrayAdapter<String> makeAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_list_item_1, carMake);
+        ArrayAdapter<String> modelAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_list_item_1, carModel);
+        ArrayAdapter<String> yearAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_spinner_dropdown_item, carYear);
+
+
+
+        makeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        makeSpin.setAdapter(makeAdapter);
+
+        modelAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        modelSpin.setAdapter(modelAdapter);
+
+        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        yearSpin.setAdapter(yearAdapter);
+
+        makeSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                String make = carMake[pos];
+                currentCar.setMake(make);
+            }
+
+            @Override
+
+            public void onNothingSelected(AdapterView<?> arg0) {
+                String make = carMake[0];
+                currentCar.setMake(make);
+
+            }
+        });
+
+        modelSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                String make = carModel[pos];
+                currentCar.setModel(make);
+            }
+
+            @Override
+
+            public void onNothingSelected(AdapterView<?> arg0) {
+                String make = carModel[0];
+                currentCar.setModel(make);
+
+            }
+        });
+
+        yearSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                String make = carYear[pos];
+                currentCar.setYear(make);
+            }
+
+            @Override
+
+            public void onNothingSelected(AdapterView<?> arg0) {
+                String make = carYear[0];
+                currentCar.setYear(make);
+
+            }
+        });
+    }
+
+
+    private void setupAcceptButton() {
+        Button accBtn = (Button) findViewById(R.id.btnOkAddCar);
+        accBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent returnValueIntent = getIntent();
+                int index = returnValueIntent.getIntExtra("carIndex",-1);
+                EditText carNameEntry = (EditText) findViewById(R.id.editNickname);
+                String carName = carNameEntry.getText().toString();
+                if(carName.length()==0 || carName==null){
+                    Toast.makeText(getApplicationContext(),"Please enter a Nickname for the car.",Toast.LENGTH_LONG).show();
+                }
+                else{
+                        currentCar.setNickname(carName);
+                        CarbonModel.getInstance().addCarToAllCar(currentCar);
+                        if(index == -1){
+                            CarbonModel.getInstance().addCarToCollecton(currentCar);
+                        }
+                        else{
+                            CarbonModel.getInstance().changeCarInCollection(currentCar,index);
+                        }
+                        finish();
+                        startActivity(new Intent(getApplicationContext(), SelectRouteActivity.class));
+                    }
+                }
+        });
+    }
+
+
 }
--- app/src/main/java/com/sfu276assg1/yancao/carbontracker/SelectTransModeActivity.java
+++ app/src/main/java/com/sfu276assg1/yancao/carbontracker/SelectTransModeActivity.java
@@ -1,13 +1,94 @@
 package com.sfu276assg1.yancao.carbontracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SelectTransModeActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> adapter;
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_trans_mode);
+
+        list = (ListView) findViewById(R.id.listView_carList);
+        registerForContextMenu(list);
+        setLaunchNewCar();
+        selectExistingCar();
+        routeList();
+    }
+
+    private void selectExistingCar(){
+        ListView list = (ListView) findViewById(R.id.listView_carList);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
+                TextView textView = (TextView) viewClicked;
+                String message = "You have chosen:  " + textView.getText().toString();
+                Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+                Car car = CarbonModel.getInstance().getCarFromCollection(position);
+                CarbonModel.getInstance().addCarToAllCar(car);
+                startActivity(new Intent(getApplicationContext(),SelectRouteActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void setLaunchNewCar() {
+        Button newRouteBtn = (Button) findViewById(R.id.btn_addCar);
+        newRouteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddCarActivity.class);
+                intent.putExtra("carIndex", -1);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void routeList() {
+        adapter = new ArrayAdapter<>(this,R.layout.route_list, CarbonModel.getInstance().getCarCollection().getCarDescription());
+        list = (ListView) findViewById(R.id.listView_carList);
+        list.setAdapter(adapter);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.select_route_context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.delete:
+                CarbonModel.getInstance().removeCarFromCollection(info.position);
+                adapter.notifyDataSetChanged();
+                routeList();
+                return true;
+            case R.id.edit:
+                Intent intent = new Intent(getApplicationContext(), AddCarActivity.class);
+                intent.putExtra("carIndex", info.position);
+                startActivity(intent);
+                finish();
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
