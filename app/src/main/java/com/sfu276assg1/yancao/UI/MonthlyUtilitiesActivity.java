8bb73d2e8f26c294c154ed58964582356b10cacc
Steven Ho <xuanhung1017@gmail.com>
2017-03-20 02:24:18 -0700

    Add Bill and BillCollection class Add MonthlyUtilitiesActivity and AddBillActivity

---
+++ app/src/main/java/com/sfu276assg1/yancao/UI/MonthlyUtilitiesActivity.java
@@ -0,0 +1,122 @@
+package com.sfu276assg1.yancao.UI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.sfu276assg1.yancao.carbontracker.Bill;
+import com.sfu276assg1.yancao.carbontracker.CarbonModel;
+import com.sfu276assg1.yancao.carbontracker.R;
+
+public class MonthlyUtilitiesActivity extends AppCompatActivity {
+
+    ArrayAdapter<Bill> adapter;
+    ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monthly_utilities);
+
+        list = (ListView) findViewById(R.id.billsListView);
+
+        setUpAddBillButton();
+        populateListView();
+        registerForContextMenu(list);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(MonthlyUtilitiesActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void setUpAddBillButton() {
+        Button button = (Button) findViewById(R.id.addBillBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MonthlyUtilitiesActivity.this, AddBillActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void populateListView() {
+        adapter = new MyListAdapter();
+        list = (ListView) findViewById(R.id.billsListView);
+        list.setAdapter(adapter);
+    }
+
+    private class MyListAdapter extends ArrayAdapter<Bill> {
+        public MyListAdapter() {
+            super(MonthlyUtilitiesActivity.this, R.layout.bill_view, CarbonModel.getInstance().getBillCollection().getBills());
+        }
+
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View itemView = convertView;
+            if (itemView == null) {
+                itemView = getLayoutInflater().inflate(R.layout.bill_view,parent, false);
+            }
+
+            Bill currentBill = CarbonModel.getInstance().getBill(position);
+
+            TextView startDateText = (TextView) itemView.findViewById(R.id.txt_StartDate);
+            startDateText.setText(currentBill.getStartDate());
+
+            TextView endDateText = (TextView) itemView.findViewById(R.id.txt_EndDate);
+            endDateText.setText(currentBill.getEndDate());
+
+            TextView peopleText = (TextView) itemView.findViewById(R.id.txt_People);
+            peopleText.setText("" + currentBill.getPeople());
+
+            TextView electricityText = (TextView) itemView.findViewById(R.id.txt_Electricity);
+            electricityText.setText("" + Double.toString(currentBill.getElectricity()));
+
+            TextView gasText = (TextView) itemView.findViewById(R.id.txt_Gas);
+            gasText.setText("" + Double.toString(currentBill.getGas()));
+
+            return itemView;
+        }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.edit_delete_context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.delete:
+                CarbonModel.getInstance().removeCar(info.position);
+                adapter.notifyDataSetChanged();
+                populateListView();
+                return true;
+            case R.id.edit:
+                Intent intent = new Intent(getApplicationContext(), AddBillActivity.class);
+                intent.putExtra("billIndex", info.position);
+                startActivity(intent);
+                finish();
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/sfu276assg1/yancao/carbontracker/Bill.java
@@ -0,0 +1,54 @@
+package com.sfu276assg1.yancao.carbontracker;
+
+/**
+ * Store information about a single bill
+ */
+
+public class Bill {
+    private String startDate;
+    private String endDate;
+    private double electricity;
+    private double gas;
+    private int people;
+
+    public Bill(String startDate, String endDate, double electricity, double gas, int people) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.electricity = electricity;
+        this.gas = gas;
+        this.people = people;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+    public String getEndDate() {
+        return endDate;
+    }
+    public double getGas() {
+        return gas;
+    }
+    public double getElectricity() {
+        return electricity;
+    }
+    public int getPeople() {
+        return people;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+    public void setGas(double gas) {
+        this.gas = gas;
+    }
+    public void setElectricity(double electricity) {
+        this.electricity = electricity;
+    }
+    public void setPeople(int people) {
+        this.people = people;
+    }
+}
+
