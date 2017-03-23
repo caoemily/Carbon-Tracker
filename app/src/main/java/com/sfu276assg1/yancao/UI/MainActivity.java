b8efe8fd2f6b82475e033e401e706dc4f971df82
Steven Ho <xuanhung1017@gmail.com>
2017-03-22 22:32:11 -0700

    Edit Tip button

--- app/src/main/java/com/sfu276assg1/yancao/UI/MainActivity.java
+++ app/src/main/java/com/sfu276assg1/yancao/UI/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -115,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setUpNextTips() {
-        Button button = (Button) findViewById(R.id.nextTipsBtn);
+        ImageButton button = (ImageButton) findViewById(R.id.nextTipsBtn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
